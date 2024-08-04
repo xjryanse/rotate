f80f6fab76e43b67c1b6e18a3ee66554db0b3848
xieJialun <36151536+xjryanse@users.noreply.github.com>
2024-08-05 05:34:07 +0800

    1

--- src/service/RotateAwardValueDistributeService.php
+++ src/service/RotateAwardValueDistributeService.php
@@ -8,7 +8,12 @@ class RotateAwardValueDistributeService
 {
     use \xjryanse\traits\InstTrait;
     use \xjryanse\traits\MainModelTrait;
+    use \xjryanse\traits\MainModelRamTrait;
+    use \xjryanse\traits\MainModelCacheTrait;
+    use \xjryanse\traits\MainModelCheckTrait;
+    use \xjryanse\traits\MainModelGroupTrait;
     use \xjryanse\traits\MainModelQueryTrait;
+
 
     protected static $mainModel;
     protected static $mainModelClass    = '\\xjryanse\\rotate\\model\\RotateAwardValueDistribute';
--- src/service/RotateLimitDtlService.php
+++ src/service/RotateLimitDtlService.php
@@ -8,7 +8,12 @@ class RotateLimitDtlService
 {
     use \xjryanse\traits\InstTrait;
     use \xjryanse\traits\MainModelTrait;
+    use \xjryanse\traits\MainModelRamTrait;
+    use \xjryanse\traits\MainModelCacheTrait;
+    use \xjryanse\traits\MainModelCheckTrait;
+    use \xjryanse\traits\MainModelGroupTrait;
     use \xjryanse\traits\MainModelQueryTrait;
+
 
     protected static $mainModel;
     protected static $mainModelClass    = '\\xjryanse\\rotate\\model\\RotateLimitDtl';
